4c1f1675863a2a19b0889444fccf74ba699f0088
royalpinto007 <royalpinto007@gmail.com>
2023-11-09 14:18:39 +0530

    feat: setup app

---
+++ app/src/components/Hero.jsx
@@ -0,0 +1,23 @@
+export default function Example() {
+  return (
+    <div className="bg-black pr-40 pt-5 pl-40">
+      <div className="flex items-center justify-center">
+
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
+            <span style={{ color: "#FF9933" }}>Bharat</span>
+            <span style={{ color: "#FFFFFF" }}>Go</span>
+            <span style={{ color: "#138808" }}>Random</span>
+          </h1>
+          <p>
+            <div className="text-center pt-2">
+              <span style={{ color: "#FFFFFF" }}>
+                Connect with Strangers and make new friends!
+              </span>
+            </div>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
